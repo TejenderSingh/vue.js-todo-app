95d4e976ffd1fa66c012e7184fff2227a90f3635
TejenderSingh <singhtj22@gmail.com>
2019-02-28 23:54:41 +1300

    added simple form validation

--- src/App.vue
+++ src/App.vue
@@ -89,7 +89,6 @@ export default {
 <style>
 html {
   margin: 0;
-  padding: 0;
   height: 100vh;
 }
 body {
--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -4,9 +4,14 @@
       <b-field grouped position="is-centered" class="formStyle">
         <b-input size="is-medium" v-model="title" rounded placeholder="What needs to be done?"></b-input>
         <p class="control mt">
-          <button class="button is-primary is-medium is-rounded is-outlined">Add Todo</button>
+          <input
+            type="submit"
+            value="Add Todo"
+            class="button is-primary is-medium is-rounded is-outlined"
+          >
         </p>
       </b-field>
+      <div class="errors" v-for="(error, index) in errors" :key="index">{{ error }}</div>
     </section>
   </form>
 </template>
@@ -17,18 +22,26 @@ export default {
   name: "TodoForm",
   data() {
     return {
-      title: ""
+      title: "",
+      errors: []
     };
   },
   methods: {
     addTodo() {
-      const newTodo = {
-        id: shortid.generate(),
-        title: this.title,
-        completed: false
-      };
-      this.$emit("add-todo", newTodo);
-      this.title = "";
+      this.errors = [];
+      if (!this.title) {
+        this.errors.push("Please enter some text");
+      }
+      if (!this.errors.length) {
+        const newTodo = {
+          id: shortid.generate(),
+          title: this.title,
+          completed: false
+        };
+        this.$emit("add-todo", newTodo);
+        this.title = "";
+        this.errors = [];
+      }
     }
   }
 };
@@ -37,6 +50,11 @@ export default {
 <style scoped>
 section {
   margin: 3rem 0;
+}
+.errors {
+  display: flex;
+  justify-content: center;
+  color: red;
 }
 @media screen and (max-width: 420px) {
   .formStyle {
